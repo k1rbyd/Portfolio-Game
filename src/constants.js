7546082eb2833d8672b69bdc13523c543ac1ea9c
Kaushik D <k1rby@Kaushiks-MacBook-Air.local>
2024-10-22 22:59:28 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. Thesedays, my days start and end with coding.
+    I like C and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/k1rbyd" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree in progress! It's like a quest in a video game—leveling up my brain power one theory at a time. Sure, it's a bit theoretical, but I'm gathering all the skills to conquer the coding universe!`,
+  "sofa-table": `That's my sofa. I like to relax here and watch TV.`,
+  tv: `Behold my entertainment hub! This is where I dive into the magical realms of YouTube, binge-watch TV shows, and enjoy epic movie marathons. It's the ultimate stress buster and a perfect way to bond with my family. From tech wizards to cinematic adventures, this screen brings us together for all the laughs and good times!`,
+  bed: `Welcome to my idea incubator! This is where I catch some Z's and occasionally hatch brilliant ideas. When inspiration strikes, I have to jot it down ASAP, or my brain turns into a restless hamster wheel! It's like a brainstorming session with my pillow—who needs a whiteboard?`,
+  resume: `This is my desk and on it is my resume. <a href="https://visitmypersonalresumehub.tiiny.site/" target="_blank">Check it out?</a>
+  If you have any exciting job opportunities, feel free to reach out to me at <a href="mailto:kirbysasuke@gmail.com">kirbysasuke@gmail.com</a>. Let's team up and create something awesome!`,
+  projects: `Welcome to my coding playground! This portfolio is my debut in the enchanting world of game development, crafted with the help of the Kaboom.js library. 
+  As I embark on this exciting adventure, you can explore my previous coding escapades: visit <a href="https://github.com/k1rbyd/Code-Harbor" target="_blank">Code Harbor</a> to see the codes I've tinkered with over the years, 
+  and swing by <a href="https://github.com/k1rbyd/Lunar-Pursuits" target="_blank">Lunar Pursuits</a> for some quick projects that were born out of late-night inspiration. Join me on this fun-filled journey into the world of game dev!`, 
+  library: `There are a lot of books on my shelves.
+  I probably only read a few of them. Who else compulsively buys books without ever finishing them?`,
+  exit: `If you wanna leave, feel free to close this tab.`,
+};
